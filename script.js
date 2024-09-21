2a6f8c36fda8869dd51b2aa09d3f78cf4ca2e0b0
Pranil Chavda <pranilchavda@gmail.com>
2024-09-21 09:51:56 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+const form1 = document.querySelector("form");
+const message1 = document.querySelector("#message");
+const inps = document.querySelectorAll('input[type="text"]');
+
+const h4= document.querySelector("h4");
+
+form1.addEventListener("submit", function(event){
+    event.preventDefault();
+    for(let i =0; i<inps.length;i++){
+        if(inps[i].values.trim() === ''){
+            console.log
+        }else{
+    
+        }
+    }
+   
+}
+
+);
